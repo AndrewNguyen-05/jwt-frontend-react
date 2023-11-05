e89a3315cf491ba58cb7ece7f4076140c3615be2
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-05 21:19:33 +0700

    Responsive form login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,49 @@
 import React from "react";
+import "./Login.scss";
 
 const Login = (props) => {
-  return <div className="login-container">login</div>;
+  return (
+    <div className="login-container">
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">Hoi Dan IT</div>
+            <div className="details">
+              Hoi Dan IT helps you connect and share with the people in your
+              life.
+            </div>
+          </div>
+          <div className="content-right col-sm-5 col-12 d-flex flex-column py-3 gap-2">
+            <div className="d-sm-none brand">Hoi dan IT</div>
+            <input
+              type="text"
+              className="input form-control"
+              placeholder="Email address or phone number"
+            />
+            <input
+              type="password"
+              className="input form-control"
+              placeholder="Password"
+            />
+            <button className="login btn btn-primary mt-2 fs-5 py-2 fw-bold">
+              Log in
+            </button>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                Fogotten password?
+              </a>
+            </span>
+            <hr />
+            <div className="text-center">
+              <button className="create btn py-2 fw-bold">
+                Create new account
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
